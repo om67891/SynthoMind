463fcfaca2323ca3dc3fa4959d87892e209b2734
Om8624870806 <158021399+Om8624870806@users.noreply.github.com>
2025-09-24 19:13:05 +0530

    Minor Fixes Done

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -6,8 +6,22 @@ const bcrypt = require('bcrypt');
 
 
 
-router.get('/', (req, res)=>{
-  res.render('profile');
+router.get('/', async (req, res) => {
+  try {
+    // You should have user info in session after login, e.g. req.session.user.email
+    const userEmail = req.session?.user?.email;
+    if (!userEmail) {
+      return res.redirect('/auth/login');
+    }
+    const user = await userInfo.findOne({ email: userEmail });
+    if (!user) {
+      return res.redirect('/auth/login');
+    }
+    res.render('Profile', { user });
+  } catch (err) {
+    console.error('Profile fetch error:', err);
+    res.status(500).send('Server error');
+  }
 });
 
 
