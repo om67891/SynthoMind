8a40ff6afff39497c47810919b8bae676ddd49c7
VipulPhatanagare <vipulphatangare3@gmail.com>
2025-09-24 12:29:48 +0530

    first commit to back end

---
+++ Backend/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+const { connectDB } = require("./database/db");
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo"); 
+
+connectDB();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+
+app.use(session({
+  secret: process.env.session_secret_key,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI, 
+    collectionName: "sessions",      
+    ttl: 5 * 24 * 60 * 60            
+  }),
+  cookie: {
+    secure: false, 
+    maxAge: 5 * 24 * 60 * 60 * 1000  
+  }
+}));
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
